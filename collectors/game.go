1448edcde552a1f2f0096f92a5cf4e4470db89b2
andrej.delmar <andrej.delmar@gmail.com>
2024-05-20 16:47:22 +0200

    feat: adjust game struct attribute types and parsing

--- collectors/game.go
+++ collectors/game.go
@@ -1,6 +1,7 @@
 package collectors
 
 import (
+	"strconv"
 	"strings"
 	"time"
 
@@ -9,14 +10,15 @@ import (
 )
 
 type Game struct {
-	Name          string `json:"name"`
-	Price         string `json:"price"`
-	OriginalPrice string `json:"originalPrice"`
-	URL           string `json:"url"`
-	Description   string `json:"description"`
-	Rating        string `json:"rating"`
-	NumOfRatings  string `json:"ratingsNum"`
-	Expiration    string `json:"expiration"`
+	Name          string    `json:"name"`
+	Price         float32   `json:"price"`
+	Currency      string    `json:"currency"`
+	OriginalPrice float32   `json:"originalPrice"`
+	URL           string    `json:"url"`
+	Description   string    `json:"description"`
+	Rating        float32   `json:"rating"`
+	RatingsSum    int       `json:"ratingsNum"`
+	Expiration    time.Time `json:"expiration"`
 }
 
 type gameCollector struct {
@@ -37,19 +39,44 @@ func createGameCollector(domain string, out chan Game) gameCollector {
 
 	c.OnHTML("main", func(h *colly.HTMLElement) {
 		name := h.ChildText("div.pdp-game-title h1")
-		price := h.ChildText("span[data-qa=\"mfeCtaMain#offer0#finalPrice\"]")
-		originalPrice := h.ChildText("span[data-qa=\"mfeCtaMain#offer0#originalPrice\"]")
-		ratingText := h.ChildText("span[data-qa=\"mfe-star-rating#overall-rating#average-rating\"]")
-		numOfRatingsText := strings.Split(h.ChildText("span[data-qa=\"mfe-star-rating#overall-rating#total-ratings\"]"), " ")[0]
-		description := h.ChildText("p[data-qa=\"mfe-game-overview#description\"]")
-		expirationText := h.ChildText("span[data-qa=\"mfeCtaMain#offer0#discountDescriptor\"]")
-		expiration, err := toExpiration(expirationText)
+
+		priceText := h.ChildText("span[data-qa=\"mfeCtaMain#offer0#finalPrice\"]")
+		currency := getCurrency(priceText)
+		price, err := getPrice(priceText)
 		if err != nil {
-			log.Error().Str("expiration", expirationText).Str("game", name).Err(err).Msg("Failed to parse game expiration time")
+			log.Error().Str("priceText", priceText).Str("game", name).Err(err).Msg("Failed to parse game priceText")
 			return
 		}
 
-		game := Game{name, price, originalPrice, url, description, ratingText, numOfRatingsText, expiration}
+		origPriceText := h.ChildText("span[data-qa=\"mfeCtaMain#offer0#originalPrice\"]")
+		originalPrice, err := getPrice(origPriceText)
+		if err != nil {
+			log.Error().Str("origPriceText", origPriceText).Str("game", name).Err(err).Msg("Failed to parse game origPriceText")
+			return
+		}
+
+		ratingText := h.ChildText("span[data-qa=\"mfe-star-rating#overall-rating#average-rating\"]")
+		rating, err := strconv.ParseFloat(ratingText, 32)
+		if err != nil {
+			log.Error().Str("ratingText", ratingText).Str("game", name).Err(err).Msg("Failed to parse game ratingText")
+			return
+		}
+
+		ratingsSumText := h.ChildText("span[data-qa=\"mfe-star-rating#overall-rating#total-ratings\"]")
+		ratingsSum, err := strconv.Atoi(strings.Split(h.ChildText("span[data-qa=\"mfe-star-rating#overall-rating#total-ratings\"]"), " ")[0])
+		if err != nil {
+			log.Error().Str("ratingsSumText", ratingsSumText).Str("game", name).Err(err).Msg("Failed to parse game ratingsSumText")
+			return
+		}
+
+		description := h.ChildText("p[data-qa=\"mfe-game-overview#description\"]")
+		expiration, err := toExpiration(h.ChildText("span[data-qa=\"mfeCtaMain#offer0#discountDescriptor\"]"))
+		if err != nil {
+			log.Error().Time("expiration", expiration).Str("game", name).Err(err).Msg("Failed to parse game expiration time")
+			return
+		}
+
+		game := Game{name, price, currency, originalPrice, url, description, float32(rating), ratingsSum, expiration}
 
 		log.Info().Any("game", game).Msg("Collected game")
 		out <- game
@@ -60,28 +87,42 @@ func createGameCollector(domain string, out chan Game) gameCollector {
 	return collector
 }
 
-func toExpiration(expirationText string) (string, error) {
+func toExpiration(expirationText string) (time.Time, error) {
 	dateText := strings.Replace(expirationText, "Offer ends ", "", 1)
 
 	expiration, err := time.Parse("1/2/2006 15:04 MST", dateText)
 	if err == nil {
-		return expiration.Format(time.RFC3339), nil
+		return expiration, nil
 	}
 
 	expiration, err = time.Parse("2/1/2006 15:04 MST", dateText)
 	if err == nil {
-		return expiration.Format(time.RFC3339), nil
+		return expiration, nil
 	}
 
 	expiration, err = time.Parse("1/2/2006 15:04 PM MST", dateText)
 	if err == nil {
-		return expiration.Format(time.RFC3339), nil
+		return expiration, nil
 	}
 
 	expiration, err = time.Parse("2/1/2006 15:04 PM MST", dateText)
 	if err == nil {
-		return expiration.Format(time.RFC3339), nil
+		return expiration, nil
 	}
 
-	return "", err
+	return time.Time{}, err
+}
+
+func getPrice(priceText string) (float32, error) {
+	priceNumText := strings.Join(strings.Split(priceText, "")[1:], "")
+	price, err := strconv.ParseFloat(priceNumText, 32)
+	if err != nil {
+		return float32(0), err
+	}
+
+	return float32(price), nil
+}
+
+func getCurrency(priceText string) string {
+	return strings.Split(priceText, "")[0]
 }
--- collectors/game_test.go
+++ collectors/game_test.go
@@ -36,3 +36,39 @@ func TestExpirationParser(t *testing.T) {
 		fmt.Println(date)
 	}
 }
+
+func TestGetPrice(t *testing.T) {
+	priceTexts := []struct {
+		text     string
+		expected float32
+		isNum    bool
+	}{
+		{
+			text:     "$123.456",
+			expected: 123.456,
+			isNum:    true,
+		},
+		{
+			text:     "not valid",
+			expected: 0,
+			isNum:    false,
+		},
+	}
+
+	for _, v := range priceTexts {
+		price, err := getPrice(v.text)
+		if !v.isNum {
+			assert.NotNil(t, err)
+			return
+		}
+
+		assert.Nil(t, err)
+		assert.Equal(t, v.expected, price)
+	}
+}
+
+func TestGetCurrency(t *testing.T) {
+	priceText := "$123.456"
+	currency := getCurrency(priceText)
+	assert.Equal(t, "$", currency)
+}
