d8cc721dbc1745dafb167bd03cb9a2175b239e58
andrej.delmar <andrej.delmar@gmail.com>
2024-06-09 17:35:28 +0200

    feat: add db not null constraints

--- resources/pg_init.sql
+++ resources/pg_init.sql
@@ -3,22 +3,22 @@ CREATE EXTENSION vector;
 -- The embedding model we use (all-MiniLM-L6-v2) uses 384 dimensions
 CREATE TABLE games (
     id bigserial PRIMARY KEY,
-    name text,
+    name text NOT NULL,
     description text,
-    price real,
+    price real NOT NULL,
     original_price real,
-    url text,
-    rating real,
-    rating_sum int,
+    url text NOT NULL,
+    rating real NOT NULL,
+    rating_sum int NOT NULL,
     expiration timestamp
 );
 
 -- The embedding model we use (all-MiniLM-L6-v2) uses 384 dimensions
 CREATE TABLE game_embeddings (
     id bigserial PRIMARY KEY,
-    game_id bigint,
+    game_id bigint NOT NULL,
     property_name text,
-    embedding vector(384)
+    embedding vector(384) NOT NULL
 );
 
 ALTER TABLE game_embeddings ADD CONSTRAINT fk_game_embedding FOREIGN KEY (game_id) REFERENCES games(id) ON DELETE CASCADE;
