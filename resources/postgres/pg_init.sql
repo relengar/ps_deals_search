d3243b79d1f3975068194f6c8a9e0f3481f43dfc
andrej.delmar <andrej.delmar@gmail.com>
2024-06-10 11:45:53 +0200

    feat: make postgres docker data persistent to ease local development

--- resources/postgres/pg_init.sql
+++ resources/postgres/pg_init.sql
@@ -1,7 +1,7 @@
 CREATE EXTENSION vector;
 
 -- The embedding model we use (all-MiniLM-L6-v2) uses 384 dimensions
-CREATE TABLE games (
+CREATE TABLE IF NOT EXISTS games (
     id bigserial PRIMARY KEY,
     name text NOT NULL,
     description text,
@@ -14,11 +14,12 @@ CREATE TABLE games (
 );
 
 -- The embedding model we use (all-MiniLM-L6-v2) uses 384 dimensions
-CREATE TABLE game_embeddings (
+CREATE TABLE IF NOT EXISTS game_embeddings (
     id bigserial PRIMARY KEY,
     game_id bigint NOT NULL,
     property_name text,
     embedding vector(384) NOT NULL
 );
 
+ALTER TABLE game_embeddings DROP CONSTRAINT IF EXISTS fk_game_embedding;
 ALTER TABLE game_embeddings ADD CONSTRAINT fk_game_embedding FOREIGN KEY (game_id) REFERENCES games(id) ON DELETE CASCADE;
